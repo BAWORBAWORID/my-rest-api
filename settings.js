48e5b24b118571280302d1b65b1166bea2ba5679
BAWORID <fazzafabianahmad@my.id>
2023-11-01 20:38:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Zero-Api'// yourname
+global.MONGO_DB_URI = "mongodb+srv://admin:admin@myrestapi.kqcxquj.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "" //email
 global.email_password = "" //application password email
